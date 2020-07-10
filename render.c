b9289346912101c6a35010c365d1861e1137b130
Marcus Blücher <marcusb_ali@hotmail.com>
2020-07-10 23:03:53 +0200

    scale

--- render.c
+++ render.c
@@ -1,10 +1,11 @@
 
-static void RenderRect(v2 pos, v2 scale, v4 col) {
+static void RenderRect(v2 pos, v2 size, v4 col) {
+    size = v2_Scale(size, 0.5f);
     glColor4ub(col.r, col.g, col.b, col.a);
     glBegin(GL_QUADS);
-    glVertex2f(pos.x - scale.x, pos.y - scale.y);
-    glVertex2f(pos.x + scale.x, pos.y - scale.y);
-    glVertex2f(pos.x + scale.x, pos.y + scale.y);
-    glVertex2f(pos.x - scale.x, pos.y + scale.y);
+    glVertex2f(pos.x - size.x, pos.y - size.y);
+    glVertex2f(pos.x + size.x, pos.y - size.y);
+    glVertex2f(pos.x + size.x, pos.y + size.y);
+    glVertex2f(pos.x - size.x, pos.y + size.y);
     glEnd();
 }
