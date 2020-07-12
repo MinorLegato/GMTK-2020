9c35c18c692a92cbfee5eaa0fc1e13907ec4552a
Anton Siderud <anton.siderud@gmail.com>
2020-07-12 20:22:54 +0200

    fix

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-gcc main.c -O3 -s -std=c11 -lmingw32 -lglfw3 -lopengl32 -lglu32 -lgdi32 -ldsound -mwindows
+gcc -static main.c -O3 -s -std=c11 -lmingw32 -lglfw3 -lopengl32 -lglu32 -lgdi32 -ldsound -mwindows
